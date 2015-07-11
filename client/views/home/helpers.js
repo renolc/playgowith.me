b74842217685fb686efd49bc8eed7ffb4a857a3d
Phillip Gibson <gibson.phillip.j@gmail.com>
2015-07-11 12:57:25 -0400

    Use _id param to redirect instead of string concatination.

--- client/views/home/helpers.js
+++ client/views/home/helpers.js
@@ -1,7 +1,7 @@
 Template.home.events({
   'click button': function () {
     Meteor.call('createGame', function (error, result) {
-      Router.go('/play/' + result);
+      Router.go('play', { _id: result });
     });
   }
 });
