376449268d59980ff415c8bad70605db0665f714
Phillip Gibson <gibson.phillip.j@gmail.com>
2015-07-19 23:00:39 -0400

    Move buttons into their own component.

---
+++ client/views/play/buttons/helpers.js
@@ -0,0 +1,22 @@
+Template.buttons.helpers({
+
+  // get the text for the action button
+  actionButtonText: function() {
+    switch(this.game().phase) {
+      case 'play':
+        return 'Pass';
+        break;
+
+      case 'mark':
+        if (this.player.isTurn() && this.game().readyToApprove) {
+          return 'Approve';
+        } else {
+          return 'Submit';
+        }
+        break;
+
+      default:
+        return 'bork';
+    }
+  }
+});
--- client/views/play/helpers.js
+++ client/views/play/helpers.js
@@ -245,29 +245,6 @@ Template.play.events({
   }
 });
 
-Template.play.helpers({
-
-  // get the text for the action button
-  actionButtonText: function() {
-    switch(this.game().phase) {
-      case 'play':
-        return 'Pass';
-        break;
-
-      case 'mark':
-        if (this.isTurn() && this.game().readyToApprove) {
-          return 'Approve';
-        } else {
-          return 'Submit';
-        }
-        break;
-
-      default:
-        return 'bork';
-    }
-  }
-});
-
 // get the size in pixels of each cell
 function getCellSize() {
   return gameCanvas.offsetWidth / this.game.board.length;
