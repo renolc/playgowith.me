a4251e6871e08a7fe4c35208a3426015db201a84
Phillip Gibson <gibson.phillip.j@gmail.com>
2015-08-01 19:34:51 -0400

    Add dropdown for board size.

--- client/views/home/events.js
+++ client/views/home/events.js
@@ -2,7 +2,9 @@ Template.home.events({
 
   // if the play button is clicked, create a new game then navigate to the black player url
   'click button': function () {
-    Meteor.call('createGame', function (error, result) {
+    var size = $('.ui.dropdown')[0].value;
+
+    Meteor.call('createGame', size, function (error, result) {
       Router.go('play', { _id: result });
     });
   }
