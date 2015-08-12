8eb4c9dec916e6e6c8d4a1c5baf2a648a1023cb6
Phillip Gibson <gibson.phillip.j@gmail.com>
2015-08-12 15:14:15 -0400

    Removed unused leftovers.

--- client/views/play/flash/events.js
+++
@@ -1,8 +0,0 @@
-Template.flash.events({
-
-  // if the share url input is clicked, select the whole link for ease of copying
-  'click .shareUrl': function (e) {
-    var targetElement = $(e.target);
-    targetElement.select();
-  }
-});
