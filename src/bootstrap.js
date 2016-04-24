e417de6dd61275a1ecc74958ffa531d3529e3f54
Phillip Gibson <gibson.phillip.j@gmail.com>
2016-04-23 20:15:28 -0400

    Add index page and start initial setup for game creation.

---
+++ src/bootstrap.js
@@ -0,0 +1,16 @@
+const PouchDB = require('pouchdb')
+const db = new PouchDB('go')
+
+db.sync('https://renolc.cloudant.com/go', {
+  live: true,
+  retry: true
+})
+
+start.addEventListener('click', function() {
+  const id = Math.random().toString(16).slice(2)
+  db.put({
+    _id: id
+  }).then(function (d){
+    console.log(d.id)
+  })
+})
